ff1388787bbb6d602a9ffd45efc8f68c9618bcb1
DESKTOP-CPUILTR\Jacques <vanjaarsveld.jakkie@gmail.com>
2024-05-15 10:54:47 +0200

    added color mode switch and display current mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        colorScheme="green"
+      ></Switch>
+      <Text>{colorMode === "dark" ? "dark mode" : "light mode"}</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
